6d4cd97890d12fb85c44eeade6c94e5909bd76f4
YusufIrfansyah <m.yusuf0613fikri@gmail.com>
2023-05-16 21:07:15 +0700

    tugas selesai

--- latihan/latihan3.js
+++ latihan/latihan3.js
@@ -7,6 +7,8 @@
 */
 function printRectangle(length, height) {
     // TODO: gunakan looping untuk membuat persegi panjang
+    for(let i = 0; i < height; i++){
+        console.log("* ".repeat(length).trimEnd());
 }
-
+}
 module.exports = printRectangle
